280435ecfb70dc57c5612693cacdcab310737ee9
Orca-jm8 <ryo.y24420450@icloud.com>
2022-03-14 19:05:24 +0900

    プロキシの値を変更

--- src/laravelapp/app/Http/Middleware/TrustProxies.php
+++ src/laravelapp/app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array|string
      */
-    protected $proxies = '*';
+    protected $proxies = '35.72.203.212';
 
     /**
      * The headers that should be used to detect proxies.
