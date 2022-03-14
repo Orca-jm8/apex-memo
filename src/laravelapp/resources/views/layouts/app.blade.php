90baf17c580530185d2acff99aa4d87e80dd0f1e
Orca-jm8 <ryo.y24420450@icloud.com>
2022-03-14 20:17:23 +0900

    https対応した

--- src/laravelapp/resources/views/layouts/app.blade.php
+++ src/laravelapp/resources/views/layouts/app.blade.php
@@ -10,14 +10,23 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    @if(config('app.env') === 'production')
+        <script src="{{ secure_asset('js/app.js') }}" defer></script>
+    @else
+        <script src="{{ asset('js/app.js') }}" defer></script>
+    @endif
+    
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @if(config('app.env') === 'production')
+        <link rel="stylesheet" href="{{ secure_asset('css/app.css') }}">
+    @else
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    @endif
 </head>
 <body>
     <div id="app">
