c3736410b223b79f30f51e17145758de7d4b335a
Orca-jm8 <ryo.y24420450@icloud.com>
2022-03-23 20:26:50 +0900

    N+1問題の解消

--- src/laravelapp/app/Http/Controllers/MainController.php
+++ src/laravelapp/app/Http/Controllers/MainController.php
@@ -3,23 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Memo;
-use App\Comment;
-use App\User;
 
 class MainController extends Controller
 {
     public function index()
     {
-        $memos = Memo::all();
+        $memos = Memo::with(['user', 'comments'])->get();
 
         foreach ($memos as $memo) {
-            $user_id = $memo->user_id;
-            $user = User::where('id', $user_id)->first();
-            $user_name = $user->name;
-            $memo['name'] = $user_name;
-            $count_comments = 0;
-            $count_comments = Comment::where('memo_id', $memo->id)->count();
-            $memo['count_comments'] = $count_comments;
+            $memo['name'] = $memo->user->name;
+            $memo['count_comments'] = $memo->comments->count();
         }
 
         return view('index', ['memos' => $memos]);
